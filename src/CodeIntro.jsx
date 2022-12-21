b79145d06f14952898c41871948d59ede275f84e
Kyle Moseby <1486937+kylemoseby@users.noreply.github.com>
2022-12-21 14:38:01 -0800

    Update CodeIntro.jsx
    Misc changes.

--- src/CodeIntro.jsx
+++ src/CodeIntro.jsx
@@ -11,8 +11,9 @@ import {
 import './App.scss';
 import CodePenData from './CodePenData';
 import gistData from './gistData';
+import CodePenEmbed from './CodePenEmbed';
 
-// Common wrapper for Codepen and Gist Examples
+
 function ListItem(props) {
   return (
     <li key={props.index} className="px-3 pt-2">
@@ -22,66 +23,72 @@ function ListItem(props) {
 }
 
 function Button(props) {
+  let { colour, children } = props;
   return (
-    <button type="button" className="btn btn-primary">{props.children}</button>
+    <button type="button" className={"btn btn-sm btn-" + (colour ? colour : 'primary')}>{children}</button>
   );
+}
+
+function randomIndex() {
+  return Math.floor(Math.random * 999999999999);
 }
 
 const tagSet = new Set(CodePenData.map(d => d.tags).flat());
 
+const coolPenIDs = ['GRQOQEw', 'MWVRLWN', 'RwyrzKm', 'MWXabbG'];
+
 function Code() {
 
-  const [showTags, toggleTags] = useState(false);
+  const [showTags, toggleTags] = useState(true);
   const [showGithub, toggleGithub] = useState(true);
   const [showCodepen, toggleCodepen] = useState(true);
   const [filterTags, updateFilters] = useState([]);
 
-  function Pentags(props) {
+  function CodeTags(props) {
     return (
-      <div>{props.tags.map((tag, indx) => {
-        return (<span key={tag}>{indx !== 0 ? '/' : ''}&nbsp;{tag}&nbsp;</ span>)
+      <div className="display-inline">{props.tags.map((tag, indx) => {
+        return (<span key={indx  + '-' + randomIndex()}>{indx !== 0 ? '/' : ''}&nbsp;{tag}&nbsp;</ span>)
       })}</div>
     );
   }
 
-  function PenItem(props) {
-    const { info: { slugHash }, info: { title }, info: { tags } } = props;
-    return (
-      <Link to={'pen/' + slugHash}>
-        <h3><ImCodepen/>{title}</h3>
-        {showTags && <Pentags tags={tags} />}
-      </Link>
-    );
-  }
-
   function PenExmplMenu(props) {
-    let filtered = new Set(filterTags);
-    // CHECK LOGIC FOR LEAST AMOUNT OF ITERATIONS
-    return CodePenData.map((pen) => {
-      return (
-        <div key={pen.slugHash}>
-        {pen.tags.every(d => !filtered.has(d)) &&
-            <ListItem>
-              <PenItem info={pen} />
-            </ListItem>
-        }
-        </div>
-      );
+    return props.penData.map((pen) => {
+      let { slugHash, title, tags } = pen;
+      if (filterTags.every(d => !tags.includes(d))) {
+        return (
+          <ListItem>
+          <Link to={'pen/' + slugHash}>
+            <h3><ImCodepen/>{title}</h3>
+          </Link>
+          {showTags && <CodeTags tags={tags} />}
+        </ListItem>
+        );
+      };
     });
   }
 
-  function GistsMenu() {
-    return gistData.map((gist, indx) => {
+  function GistsMenu(props) {
+    return props.gistData.map((gist) => {
+      let { id, title, tags } = gist;
       return (
-        <ListItem key={indx}>
-        <Link to={'gist/' + gist.id}>
-          <h3><ImGithub/>{gist.title}</h3>
+        <ListItem key={id}>
+        <Link to={'gist/' + id}>
+          <h3><ImGithub/>{title}</h3>
         </Link>
+        {showTags && <CodeTags tags={tags} />}
       </ListItem>
       );
     })
   }
+  const tags = [...tagSet].map((tag, indx) => {
+    return (
+      <div key={indx + '-' + randomIndex()} className={"btn btn-" + (filterTags.includes(tag) ? "secondary" : "primary") + " btn-sm"} onClick={() => {toggleTag(tag)}}>
+        {tag}
+      </div>);
+  });
 
+  // EVENT HANDLERS
   function toggleTag(tag) {
     let filtered = new Set(filterTags);
 
@@ -94,58 +101,51 @@ function Code() {
     updateFilters([...filtered]);
   }
 
-  const tags = [...tagSet].map((tag, indx) => {
-    return (
-      <div key={indx} onClick={() => {toggleTag(tag)}}>
-        {tag}
-      </div>);
-  });
-
   function tagClick() {
     toggleTags(!showTags);
   }
 
-  function clearFilters(){
+  function clearFilters() {
+    debugger;
     updateFilters([]);
-    toggleGithub(true)
-    toggleCodepen(true)
+    // toggleGithub(true)
+    // toggleCodepen(true)
   }
-  function clickGithub(){
+
+  function clickGithub() {
     toggleGithub(!showGithub)
   }
-  function clickCodepen(){
+
+  function clickCodepen() {
     toggleCodepen(!showCodepen)
   }
+
+  let coolPen = coolPenIDs.at(Math.floor(Math.random() * coolPenIDs.length));
 
   return (
     <div className="row">
       <div className="col">
-        <h3>Tags</h3>
-        <div>{tags}</div>
+        <CodePenEmbed slugHash={coolPen} />
+        <p><Link to={'pen/' + coolPen}>Click more info.</Link></p>
+        <div className="d-flex flex-wrap">
+          <div className="btn-group btn-group-sm" role="group" aria-label="Small button group">
+            Filter by tag: {tags}
+          </div>
+        </div>
         <Button>
           <div onClick={clearFilters}>Clear Filters</div>
         </Button>
-        <Button>
-          <div onClick={clickCodepen}>
-            Codepen
-          </div>
-        </Button>
-        <Button>
-          <div onClick={clickGithub}>
-            Github Gist
-          </div>
-        </Button>
-        <Button>
+        <Button colour="primary">
           <div onClick={tagClick}>
             {showTags ? <ImEnlarge2 /> : <ImShrink2 />}
           </div>
         </Button>
         <ul className="list-unstyled">
           {showCodepen &&
-            <PenExmplMenu />
+            <PenExmplMenu penData={CodePenData} />
           }
           {showGithub &&
-            <GistsMenu/>
+            <GistsMenu gistData={gistData} />
           }
         </ul>
       </div>
