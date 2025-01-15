630add017d7f25f17aa00034a70186d3c33242d4
Kyle Moseby <1486937+kylemoseby@users.noreply.github.com>
2025-01-14 21:01:29 -0800

    New build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,6 @@ const socialLinks = linkData.map((d, i) => {
   return (<li key={i}><a href={d.url}>{d.title}</a></li>);
 });
 
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--jersey-15-regular)]">
